b639b7fb890256154534f3bbae7f4714c20124fa
arlarchuleta-art <arlarchuleta@gmail.com>
2026-07-22 06:54:15 -0600

    Add ClientStart and begin testing
    Uploaded ClientStart.java and updated documentation to begin testing phase.

---
+++ ClientStart.java
@@ -0,0 +1,14 @@
+// Name: Adam Archuleta
+// Date: July 2026
+// Course: CSCI 2251
+// Project: Networking and GUI
+// Citation: Baseline client initialization class.
+
+public class ClientStart 
+{
+    public static void main(String[] args) 
+    {
+        // Instantiating the Client handles the GUI generation
+        Client application = new Client();
+    }
+}
